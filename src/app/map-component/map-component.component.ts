17579666c34844f32cfceb5a3f4f44c68ea6cf39
Ryanj4043 <ryan.auld@yahoo.co.uk>
2020-07-13 22:24:24 +0100

    Added Icon on click
    Added Icon on click
     - TODO: get a new icon
     - TODO: remove features on click
     - TODO: Find way to export this all to JSON

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -9,6 +9,13 @@ import Static from 'ol/source/ImageStatic';
 import {defaults as defaultInteractions, DragRotateAndZoom} from 'ol/interaction';
 
 import {SizeCheckService} from "./size-check.service";
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import {transform, toLonLat} from "ol/proj"
 
 @Component({
   selector: 'app-map-component',
@@ -29,7 +36,15 @@ export class MapComponentComponent implements OnInit {
 
   public url: string[] = [];
 
-  public layers: ImageLayer[];
+  public layers: any[] = [];
+
+  public markerLayer: VectorSource;
+
+  public markerStyle = new Style({
+    image: new Icon({
+      src: 'https://icons.iconarchive.com/icons/paomedia/small-n-flat/32/pin-icon.png'
+    })
+  });
 
   constructor(private sizeCheck: SizeCheckService) { }
 
@@ -37,6 +52,7 @@ export class MapComponentComponent implements OnInit {
   }
 
   async onUpload(){
+    this.markerLayer = new VectorSource({});
     if(this.url[0]){
       let wandh = await this.sizeCheck.getImageDimenstion(this.url[0]);
       this.extent[2] = wandh[0];
@@ -59,12 +75,24 @@ export class MapComponentComponent implements OnInit {
           maxZoom: 5
         })
       });
+
+      this.map.on("singleclick", (event)=>{
+        var lonLat = toLonLat(event.coordinate);
+        this.addMarker(lonLat[0], lonLat[1]);
+      })
     }
   }
 
-  createLayers(): void{
-    this.layers = [];
+  addMarker(lon: any, lat: any): void {
+    var iconFeature = new Feature({
+      geometry: new Point(transform([lon, lat], 'EPSG:4326',
+        'EPSG:3857')),
+    });
+  
+    this.markerLayer.addFeature(iconFeature);
+  }
 
+  createLayers(): void{
     for(let i =0 ; i < 3; i++ ){
       if(this.url[i]){
         let layer = new ImageLayer({
@@ -77,6 +105,10 @@ export class MapComponentComponent implements OnInit {
         this.layers.push(layer);
       }
     }
+
+    let vLayer = new VectorLayer({source: this.markerLayer, style: this.markerStyle});
+    this.layers.push(vLayer); 
+  
   }
 
 }
